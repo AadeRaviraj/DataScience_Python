33a5a9b8b18a2e46a294af3799ded894d07917d0
Raviraj <ravirajaade299@gmail.com>
2026-08-01 17:03:33 +0530

    Add data science practice notebook scripts

--- median.py
+++ median.py
@@ -3,23 +3,30 @@ import pandas as pd
 
 
 
-border = " - " * 40
+border = "=" * 70
 
 
 arr = np.array([40, 30, 20 , 10 , 50])
 asc_order = np.sort(arr)
 
 print(border)
-print("Sorted array : ", asc_order)
+print("Sorted array (Asc Order): ", asc_order)
 print(border)
 
-desc_order = np.sort(arr)[::-1]
+desc_order = np.sort(arr)[::-1] # output should be 50 , 40 , 30, 20 , 10
 print(border)
 print("Descending order : ", desc_order )
 print(border)
 
-
 # get median value 
+
+print(border)
+print("Calculate median value of odd sequence : ")
+print(border)
+
+median_value = np.median(arr)
+print("Median Value : ",median_value)
+print(border)
 
 
 
