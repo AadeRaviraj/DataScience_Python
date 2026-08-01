4fed619b823bfaf309bc8ee284bef900053f95ad
Raviraj <ravirajaade299@gmail.com>
2026-08-01 11:12:55 +0530

    Add data science practice notebook scripts

--- mean.py
+++ mean.py
@@ -1,18 +1,31 @@
 import numpy as np 
 import pandas as pd 
 
+# create an array 
 arr = np.array([2,4,3,5,6,7,8,2,4,8,9,3,5,6,7,8,8,8])
 
+# sum of all array 
 sum = np.sum(arr)
-print(sum)
+print("Sum of all array : ",sum)
 
+
+# calculate length of array 
 length_arr = len(arr)
-print(length_arr)
+print("Length of array : ",length_arr)
 
+# calculate mean by manually 
 mean_average = sum / length_arr
+print("Manually calculate mean :", mean_average)
 
-print("Average of all values :", mean_average)
 
 
+# Using in-build method calculate mean 
+
+mean_cal = np.mean(arr)
+print("Calculate mean using in-build method : ", mean_cal)
+
+# sort data 
 sorted_Data = np.sort(arr)
-print(sorted_Data)
+print("Sorted Array ",sorted_Data)
+
+
