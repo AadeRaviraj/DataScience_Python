796e5f0181566e8f86e56a8e2a9252da95cd9ac8
Raviraj <ravirajaade299@gmail.com>
2026-08-01 10:51:53 +0530

    practice of ds

---
+++ mean.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import pandas as pd 
+
+arr = np.array([2,4,3,5,6,7,8,2,4,8,9,3,5,6,7,8,8,8])
+
+sum = np.sum(arr)
+print(sum)
+
+length_arr = len(arr)
+print(length_arr)
+
+mean_average = sum / length_arr
+
+print("Average of all values :", mean_average)
