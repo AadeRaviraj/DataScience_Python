d5ad0fdc1611e926a106e8860b1ed381c21b12e7
Raviraj <ravirajaade299@gmail.com>
2026-08-08 21:55:05 +0530

    Add data science practice notebook scripts

--- Mean_Absoulate_Deviation.py
+++ Mean_Absoulate_Deviation.py
@@ -1,8 +1,20 @@
+
+
+border = "-" * 90
+
+
 data = [10, 20, 30]
 
+print(border)
 # Number of values
 n = len(data)
 
+print(border)
+
 # Total of list
 total = sum(data)
+
+# mean calculate 
+
+meanValue = total / n
 
